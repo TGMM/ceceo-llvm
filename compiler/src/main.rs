0f95fd0a4a68e878edeab6f4758164b53ce7ef86
Eliud de León <eliud.deleon.10@gmail.com>
2022-12-11 23:30:55 -0600

    Initialized the compiler crate

---
+++ compiler/src/main.rs
@@ -0,0 +1,21 @@
+use ceceo_llvm_parser::parse_ceceo;
+use clap::Parser;
+use std::fs;
+
+#[derive(Parser, Default, Debug)]
+struct Arguments {
+    file_name: String,
+}
+
+fn main() {
+    let args = Arguments::parse();
+    match fs::read_to_string(args.file_name) {
+        Ok(contents) => {
+            let parsed_ceceo = parse_ceceo(&contents).unwrap();
+            println!("{:?}", parsed_ceceo);
+        }
+        Err(err) => {
+            println!("{}", err);
+        }
+    }
+}
